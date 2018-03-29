ec44bcc68276edfabd559ed8262258f968ff1f26
Stuart Perks <stuart.m.perks@gmail.com>
2018-03-29 21:46:01 +0100

    init.el

---
+++ init.el
@@ -0,0 +1,313 @@
+;; ---------------------------------------------------
+;; Sample emacs config focusing on clojure development
+;; ---------------------------------------------------
+
+;; installed packages
+;; - exec-path-from-shell (not from stable!)
+;; - hl-sexp
+;; - paredit
+;; - clojure-mode
+;; - cider
+;; - company
+;; - flycheck (not from stable!)
+;; - flycheck-clojure
+;; - clj-refactor
+
+;; Add .emacs.d/lisp to load-path
+(setq dotfiles-lisp-dir
+      (file-name-as-directory
+       (concat (file-name-directory
+                (or (buffer-file-name) load-file-name))
+               "lisp")))
+(add-to-list 'load-path dotfiles-lisp-dir)
+
+;; don't use tabs for indent
+(setq-default indent-tabs-mode nil)
+
+;; emacs package management
+;; use MELPA stable
+(require 'package)
+
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                         ("marmalade" . "https://marmalade-repo.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
+
+;; keep the installed packages in .emacs.d
+(setq package-user-dir (expand-file-name "elpa" user-emacs-directory))
+(package-initialize)
+;; update the package metadata is the local cache is missing
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(setq load-prefer-newer t)
+
+;; reduce the frequency of garbage collection by making it happen on
+;; each 50MB of allocated data (the default is on every 0.76MB)
+(setq gc-cons-threshold 50000000)
+
+;; warn when opening files bigger than 100MB
+(setq large-file-warning-threshold 100000000)
+
+
+
+
+(add-to-list 'load-path (concat user-emacs-directory "site-lisp"))
+
+(defun require-package (package &optional min-version no-refresh)
+  "Install given PACKAGE, optionally requiring MIN-VERSION.
+If NO-REFRESH is non-nil, the available package lists will not be
+re-downloaded in order to locate PACKAGE."
+  (if (package-installed-p package min-version)
+      t
+    (if (or (assoc package package-archive-contents) no-refresh)
+        (package-install package)
+      (progn
+        (package-refresh-contents)
+        (require-package package min-version t)))))
+
+(setq package-enable-at-startup nil) ; Don't initialize later as well
+
+
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-verbose t)
+
+
+(use-package dired
+  :config
+  ;; dired - reuse current buffer by pressing 'a'
+  (put 'dired-find-alternate-file 'disabled nil)
+
+  ;; always delete and copy recursively
+  (setq dired-recursive-deletes 'always)
+  (setq dired-recursive-copies 'always)
+
+  ;; if there is a dired buffer displayed in the next window, use its
+  ;; current subdir, instead of the current subdir of this dired buffer
+  (setq dired-dwim-target t)
+
+  ;; enable some really cool extensions like C-x C-j(dired-jump)
+  (require 'dired-x))
+
+(use-package easy-kill
+  :ensure t
+  :config
+  (global-set-key [remap kill-ring-save] 'easy-kill))
+
+(use-package exec-path-from-shell
+  :ensure t
+  :config
+  (when (memq window-system '(mac ns))
+    (exec-path-from-shell-initialize)))
+
+(use-package move-text
+  :ensure t
+  :bind
+  (([(meta shift up)] . move-text-up)
+   ([(meta shift down)] . move-text-down)))
+
+(use-package yaml-mode
+  :ensure t)
+
+(use-package cask-mode
+  :ensure t)
+
+(use-package company
+  :ensure t
+  :config
+  (global-company-mode))
+
+
+
+(use-package ivy
+  :ensure t
+  :config
+    (ivy-mode 1)
+  (setq ivy-use-virtual-buffers t)
+  (setq enable-recursive-minibuffers t)
+  (global-set-key (kbd "C-c C-r") 'ivy-resume)
+  (global-set-key (kbd "<f6>") 'ivy-resume))
+
+
+(use-package clojure-mode
+  :ensure t
+  :config
+  (add-hook 'clojure-mode-hook #'paredit-mode)
+  (add-hook 'clojure-mode-hook #'subword-mode)
+  (add-hook 'clojure-mode-hook #'rainbow-delimiters-mode))
+
+(use-package cider
+  :ensure t
+  :config
+  (add-hook 'cider-mode-hook #'eldoc-mode)
+  (add-hook 'cider-repl-mode-hook #'eldoc-mode)
+  (add-hook 'cider-repl-mode-hook #'paredit-mode)
+  (add-hook 'cider-repl-mode-hook #'rainbow-delimiters-mode))
+
+(use-package clojure-cheatsheet
+  :ensure t)
+
+(use-package clj-refactor
+  :ensure t)
+(use-package cljsbuild-mode
+  :ensure t)
+
+(use-package flycheck
+  :ensure t
+  :config
+  (add-hook 'after-init-hook #'global-flycheck-mode))
+
+(use-package flycheck-joker
+  :ensure t)
+
+(use-package helm
+  :ensure t)
+
+(use-package cljr-helm
+  :ensure t)
+
+(use-package projectile
+  :ensure t
+  :config
+  (projectile-global-mode)
+  (setq projectile-enable-caching t))
+
+(use-package helm-projectile
+  :ensure t
+  :bind ("M-t" . helm-projectile-find-file)
+  :config
+  (helm-projectile-on))
+
+(use-package rainbow-delimiters
+  :ensure t)
+
+(use-package rainbow-mode
+  :ensure t
+  :config
+  (add-hook 'prog-mode-hook #'rainbow-mode))
+
+(use-package magit
+  :ensure t)
+
+(use-package magit-annex
+  :ensure t)
+
+(use-package json-mode
+  :ensure t)
+(use-package ag
+  :ensure t)
+
+(global-linum-mode t)
+
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+;; Allow hash to be entered  
+(global-set-key (kbd "M-3") '(lambda () (interactive) (insert "#")))
+;; Theme
+(load-file "~/.emacs.d/themes/dracula2-theme.el")
+;;(dracula2)
+
+;; show opening, closing parens
+(show-paren-mode)
+
+(require-package 'epl)
+
+(require-package 'exec-path-from-shell)
+;; Sort out the $PATH for OSX
+(require 'exec-path-from-shell)
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
+
+(dolist (file '("cfg-paredit.el"
+		"cfg-flycheck.el"
+		"cfg-hlsexp.el"
+		"cfg-cider.el"
+                "cfg-cljrefactor.el"))
+  (load (concat dotfiles-lisp-dir file)))
+
+
+(require 'cider-eldoc)
+;;(require 'clj-refactor)
+
+;; helm
+(require 'helm-config)
+;;cljr-helm
+(require 'cljr-helm)
+;; global set helm
+(global-set-key (kbd "M-x") 'helm-M-x) 
+
+(autoload 'clojure-mode "clojure-mode" "clojure mode" t)
+(add-hook 'cider-mode-hook
+        (lambda () (setq next-error-function #'flycheck-next-error-function)))
+
+(add-to-list 'auto-mode-alist '("\\.clj$" . clojure-mode))
+
+(add-hook 'clojure-mode-hook 'cider-mode)
+;; Add keybindings for items
+(add-hook 'clojure-mode-hook
+         (lambda ()
+            (setq-local cider-repl-use-pretty-printing t)
+            (local-set-key [f5] 'helm-imenu)
+            (local-set-key [f6] 'cljr-helm)
+            (local-set-key (kbd "<C-f5>") 'cider-test-run-test)
+            (cider-auto-test-mode t)))
+
+(add-hook 'cider-repl-mode-hook
+          (lambda ()
+            (local-set-key [f6] 'cljr-helm)))
+
+(provide 'ca-clojure)
+
+(add-hook 'after-init-hook 'global-company-mode)
+
+;; Enable projectile mode
+(projectile-mode)
+(add-hook 'after-init-hook #'projectile-global-mode)
+
+
+;; Magit for git
+(global-set-key (kbd "C-x g") 'magit-status)
+
+
+;; Custom User configurations:
+;; If you wish to add additional functionality to your emacs config beyond what is in this setup,
+;; simply add a file called "user-customizations.el" to your .emacs.d/lisp/ directory. Within that file,
+;; you have access to the (require-package ...) function defined here, so for example, you could have:
+;; (require-package 'rainbow-delimiters)
+;; This would be all that is needed for emacs to automatically download the Rainbow Delimiters package
+;; from Melpa. Additional configs of any kind could be added to this user-customizations.el file.
+;; If the file is ommitted, no problem, no customizations are run.
+
+
+(when (file-exists-p (concat dotfiles-lisp-dir "user-customizations.el"))
+  (load (concat dotfiles-lisp-dir "user-customizations.el")))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes nil)
+ '(custom-safe-themes
+   (quote
+    ("138d69908243e827e869330c43e7abc0f70f334dfa90a589e4d8a1f98a1e29af" default)))
+ '(package-selected-packages
+   (quote
+    (json-mode cider cider-eval-sexp-fu ghub magit projectile company-irony helm ag rainbow-delimiters company hl-sexp paredit exec-path-from-shell))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; Send backups and auto saves to direcotey
+(setq
+ backup-by-copying t      ; don't clobber symlinks
+ backup-directory-alist '(("." . "~/.saves"))    ; don't litter my fs tree
+ delete-old-versions t
+ kept-new-versions 6
+ kept-old-versions 2
+ version-control t)       ; use versioned backups
+
